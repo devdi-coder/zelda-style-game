fa00021c1c85969a5a40ad51c6a89d70cddf8e64
Diego Santos <diegosfaria022@hotmail.com>
2025-03-03 14:56:32 -0300

    Initial logic

--- level.py
+++ level.py
@@ -27,7 +27,7 @@ class Level:
 
     def run(self):
         
-        self.visible_sprites.custom_draw()
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
 
 class YSortCameraGroup(pygame.sprite.Group):
@@ -35,9 +35,15 @@ class YSortCameraGroup(pygame.sprite.Group):
 
         super().__init__()
         self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
         self.offset = pygame.math.Vector2()
 
-    def custom_draw(self):
-        for sprite in self.sprites():
-            offset_rect = sprite.rect.topleft + self.offset
-            self.display_surface.blit(sprite.image,sprite.rect)
+    def custom_draw(self, player):
+        
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+
+        for sprite in sorted(self.sprites(),key = lambda sprite: sprite.rect.centery):
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image,offset_pos)
